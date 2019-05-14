f54e9c40759eb18c7d0867945eb1351f79caff5d
Andrey Lamzin <lamzin.an@gmail.com>
2019-05-14 16:49:14 +0300

    Ещё несколько заглушек

--- androidshim.es6.js
+++ androidshim.es6.js
@@ -1,10 +1,26 @@
 //Здесь просто заглушка, в окончательный проект этот код никак не попадёт
 class Intent {
+    setFlags(flags) {
 
+    }
+    putExtra(key, val) {
+        if (!this._store) {
+            this._store = {};
+        }
+        this._store[key] = val;
+    }
 }
+Intent.FLAG_ACTIVITY_NEW_TASK = 'FLAG_ACTIVITY_NEW_TASK';
+
 class Context {
     getContentResolver() {
         return this;
+    }
+    getApplication() {
+        return this;
+    }
+    startActivity(intent) {
+        
     }
 }
 
